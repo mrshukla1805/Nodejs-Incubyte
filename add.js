e5062230b1dfbcc6d6e3887fa17bbc6228753803
mrshukla1805 <nageshshukla1805@gmail.com>
2024-02-17 12:04:37 +0530

    feat: refactored code to send array of sums and testcases to recieve array of sums

--- add.js
+++ add.js
@@ -1,11 +1,15 @@
 
-function add(str) {
+function addHelper(str) {
   if (str === "") {
     return 0;
   }
   return str.split(',')
             .map(Number)
             .reduce((sum, curr) => sum + curr, 0);
+}
+
+function add(strArray){
+  return strArray.map(addHelper);
 }
 
 
