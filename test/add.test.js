5acda4158a980f516db174f5e9aa9743f29eab00
mrshukla1805 <nageshshukla1805@gmail.com>
2024-02-17 11:53:15 +0530

    feat: refactored testcases to give array of strings as input

--- test/add.test.js
+++ test/add.test.js
@@ -8,11 +8,15 @@ import add from '../add.js';
 describe('calculor function', function() {
 
   it('should return 0 for an empty string', function() {
-    expect(add("")).to.equal(0);
+    expect(add([""])).to.equal(0);
   });
 
   it('should return sum for an comma seperated string', function() {
-    expect(add("4,5,5")).to.equal(14);
+    expect(add(["4,5,5"])).to.equal(14);
+  });
+
+  it('should return sum for multiple comma seperated string', function() {
+    expect(["", "1", "1,5"]).to.equal(0,1,6);
   });
 
 });
